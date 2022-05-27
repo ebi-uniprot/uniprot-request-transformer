65ed03a2be85668f281f44d331ab63282d3346a5
Daniel Rice <dlrice@ebi.ac.uk>
2022-05-27 09:01:12 +0100

    Include status in request count.

--- count_requests.py
+++ count_requests.py
@@ -7,13 +7,19 @@ def floor_minute(timestamp):
     return 60 * int(int(timestamp) / 60)
 
 
+def get_status_key(status):
+    return f"{status}xx"
+
+
 def main():
     requests_per_minute = defaultdict(int)
-    for timestamp in sys.stdin:
+    for line in sys.stdin:
+        timestamp, status = line.split("|")
         floored = floor_minute(timestamp)
-        requests_per_minute[floored] += 1
-    for timestamp in sorted(requests_per_minute.keys()):
-        sys.stdout.write(f"{timestamp},{requests_per_minute[timestamp]}\n")
+        requests_per_minute[(floored, get_status_key(status))] += 1
+    for k in sorted(requests_per_minute.keys()):
+        (timestamp, status) = k
+        sys.stdout.write(",".join([timestamp, status, requests_per_minute[k]]))
 
 
 if __name__ == "__main__":
